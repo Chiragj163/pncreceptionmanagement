95b73899eca7ef9602138ffb0941ab63b4283653
PNC <chiragj163@gmail.com>
2026-08-31 12:06:50 +0530

    fix reception base path

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/reception/',
   plugins: [react()],
-
-  server: {
-    host: "0.0.0.0",
-    port: 5173
-  }
 });
